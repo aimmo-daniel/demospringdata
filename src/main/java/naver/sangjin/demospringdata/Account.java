1e9fa7db846127030b70310d267170feea4631c5
sangjin <sangjin0309@github.com>
2019-04-09 23:16:36 +0900

    JPA 프로그래밍 2. 엔티티 타입 맵핑

--- src/main/java/naver/sangjin/demospringdata/Account.java
+++ src/main/java/naver/sangjin/demospringdata/Account.java
@@ -1,8 +1,7 @@
 package naver.sangjin.demospringdata;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity //Account 테이블에 매핑이 된다고 알려주는
 public class Account {
@@ -10,9 +9,21 @@ public class Account {
     @Id @GeneratedValue //(주키 / 자동으로 생성되는 값을 사용하겠다)
     private Long id;
 
+    //모든 컬럼에는 @Column 에노테이션이 생략되어있으나 추가옵션을 설정하고 싶을때 직접 명시
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created = new Date();
+
+    private String yes;
+
+    @Transient //컬럼으로 매핑 하기 싫고 객체에서만 쓸때 붙여준다
+    private String no;
+
+    // getter, setter는 없어도 컬럼으로 매핑이 된다
 
     public Long getId() {
         return id;
