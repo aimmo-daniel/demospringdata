434b7d3a47f941be52f4148b1980a5a8e9ab5249
sangjin <sangjin0309@github.com>
2019-06-10 22:33:25 +0900

    스프링 데이터 Common 1. 리포지토리

--- src/main/java/naver/sangjin/demospringdata/PostRepository.java
+++ src/main/java/naver/sangjin/demospringdata/PostRepository.java
@@ -1,5 +1,7 @@
 package naver.sangjin.demospringdata;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,5 +12,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT new map (p.id AS id, p.title AS title) FROM Post p")
     List<Map<String, Object>> findAlls();
+
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 
 }
